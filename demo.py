9c5038fd11019ccd08a9cd983486511dfc8e1be7
Euleeee <Jona98welkers@web.de>
2020-10-06 21:51:35 +0200

    A few changes

--- demo.py
+++ demo.py
@@ -1,9 +1,10 @@
 import math as m
 
-def square(halfSideLength = 30):
+def square(halfSideLength = 30, robotHeight = -90):
     """Calculate coordinates for a square
-        `a`: half length of the edge
-        `retrun`: List of positions and 
+        `halfSideLength`: half length of the edge
+        `robotHeight`: z-Coordinate for 2D model (have to be a negative value)
+        `retrun`: List of positions and driving mode
         """
 #  _______ 
 # |       |
@@ -14,20 +15,21 @@ def square(halfSideLength = 30):
 
     posSquare = [
         [0,0,-70,0,0,0,'mov'],
-        [halfSideLength,halfSideLength,-90,0,0,0,'mov'],
-        [-halfSideLength,halfSideLength,-90,0,0,0,'lin'],
-        [-halfSideLength,-halfSideLength,-90,0,0,0,'lin'],
-        [halfSideLength,-halfSideLength,-90,0,0,0,'lin'],
-        [halfSideLength,halfSideLength,-90,0,0,0,'lin'],
+        [halfSideLength,halfSideLength,robotHeight,0,0,0,'mov'],
+        [-halfSideLength,halfSideLength,robotHeight,0,0,0,'lin'],
+        [-halfSideLength,-halfSideLength,robotHeight,0,0,0,'lin'],
+        [halfSideLength,-halfSideLength,robotHeight,0,0,0,'lin'],
+        [halfSideLength,halfSideLength,robotHeight,0,0,0,'lin'],
         [0,0,-127,0,0,0,'mov']
         ]
 
     return posSquare
 
 
-def triangle(halfSideLength = 15):
+def triangle(halfSideLength = 15, robotHeight = -90) :
     """Calculate coordinates for a samesided triangle
         `halfSideLength`: half sidelength of the triangle
+        `robotHeight`: z-Coordinate for 2D model (have to be a negative value)
         `retrun`: List of positions and 
         """
 #     ^ 
@@ -42,10 +44,10 @@ def triangle(halfSideLength = 15):
 
     posTriangle = [
         [0,0,-70,0,0,0,'mov'],
-        [-hHalf,halfSideLength,-90,0,0,0,'mov'],
-        [-hHalf,-halfSideLength,-90,0,0,0,'lin'],
-        [hHalf,0,-90,0,0,0,'lin'],
-        [-hHalf,halfSideLength,-90,0,0,0,'lin'],
+        [-hHalf,halfSideLength,-robotHeight,0,0,0,'mov'],
+        [-hHalf,-halfSideLength,robotHeight,0,0,0,'lin'],
+        [hHalf,0,robotHeight,0,0,0,'lin'],
+        [-hHalf,halfSideLength,robotHeight,0,0,0,'lin'],
         [0,0,-127,0,0,0,'mov']
         ]
 
